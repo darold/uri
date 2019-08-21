75696023d338b5589a09a12f7064e520042eefa7
Gilles Darold <gilles.darold@lzlabs.com>
2019-08-22 00:30:53 +0200

    Add new function uri_get_relative_path(url uri, base uri) used to return the path of a URI relative to its base.

--- README.md
+++ README.md
@@ -97,6 +97,8 @@ Other functions:
 - `uri_path_size(uri)` returns the size of a local path (not symlink) or remote url (local/remote is autodetected).
 - `uri_escape(text)` returns the encoded URL of the given string using [curl_easy_escape()](https://curl.haxx.se/libcurl/c/curl_easy_escape.html).
 - `uri_unescape(text)` returns the decoded URL of the given string using [curl_easy_unescape()](https://curl.haxx.se/libcurl/c/curl_easy_unescape.html).
+- `uri_rebase_url(uri, uri)` returns the url resulting from a uri path rebased on an other uri.
+- `uri_get_relative_path(uri, uri)` returns the path of a URI relative to its base.
 
 Normalization
 ------------
--- uri--1.0.0--1.1.0.sql
+++ uri--1.0.0--1.1.0.sql
@@ -1,1 +1,24 @@
 ALTER FUNCTION uri_rebase(uri, uri) RENAME TO uri_rebase_url(uri, uri);
+
+-- Function uri_get_relative_path() can be used to return the path
+-- of a URI relative to its base specified as second parameter.
+CREATE FUNCTION uri_get_relative_path(uri, uri) RETURNS text AS $$
+DECLARE
+	v_uri uri;
+	v_path text;
+	v_base text;
+BEGIN
+	-- Rebase the URL/path with the speficied base
+	SELECT uri_rebase_url($1, $2) INTO v_uri;
+	SELECT uri_get_path(v_uri) INTO v_path;
+	SELECT uri_get_path($2) INTO v_base;
+	-- Add a / at end of the base if this is not already the case
+	IF regexp_match(v_base, '/$') IS NULL THEN
+		v_base := v_base||'/';
+	END IF;
+	-- Remove the base from the path
+	SELECT regexp_replace(v_path, '^'||v_base, '') INTO v_path;
+	RETURN v_path;
+END
+$$ LANGUAGE plpgsql STRICT;
+
